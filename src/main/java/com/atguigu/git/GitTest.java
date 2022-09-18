087fde3d857237eff877f367f4c24ff717556ba9
lzf <lzf@qq.com>
2022-09-18 12:40:27 +0800

    test4 commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("git!");
         System.out.println("i");
+        System.out.println("x");
     }
 }
