5244918301413de65d71e742a871a9133ef1d905
lzf <lzf@qq.com>
2022-09-18 14:57:22 +0800

    test4 push commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("1");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("push test");
     }
 }
